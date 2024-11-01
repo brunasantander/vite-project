03ebaf65fdf10a75aaf08c093119abf11d43bdd0
brunasantander <brunasantander.2011@gmail.com>
2024-11-01 14:26:59 -0300

    pagina pronta

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,34 @@
+import { Button, Image, List, ListItem, Nav, NavLink, Wrapper } from "./navbar.styles";
+import logoFipp from "../../assets/images/logo-fipp.png";
+import logoUnoeste from "../../assets/images/logo-unoeste.png";
+import { MobileNavbar } from "./mobile-navbar";
+
+export const Navbar = () => {
+    const openLink = () => {
+        const url = "https://www.unoeste.br/";
+        window.open(url, '_blank');
+    };
+
+    return (
+        <Wrapper>
+            <Nav>
+                <Image src={logoFipp} alt = "logo fipp"/>
+                <List>
+                    <ListItem>
+                        <NavLink to="/">Home</NavLink>
+                    </ListItem>
+                    <ListItem>
+                        <NavLink to="/corpo-docente">Corpo Docente</NavLink>
+                    </ListItem>
+                    <ListItem>
+                        <NavLink to="/contato">Contato</NavLink>
+                    </ListItem>
+                    <ListItem>
+                        <Button onClick={openLink}><Image src={logoUnoeste} alt="logo unoeste"/></Button>
+                    </ListItem>
+                </List>
+                <MobileNavbar />
+            </Nav>
+        </Wrapper>
+    );
+}
